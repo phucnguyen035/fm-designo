161a0cf70a559b5f97b6e8d1235f86ac738a742b
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2022-08-27 23:15:28 +0700

    feat: add astro solid, image, remove react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
-import react from '@astrojs/react'
+import image from '@astrojs/image'
+import solidJs from '@astrojs/solid-js'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), sitemap(), react()],
+	site: 'https://fm-designo.nguyenhphuc.com',
+	integrations: [tailwind(), sitemap(), image(), solidJs()],
 })
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,6 @@
-/// <reference types="astro/client" />
+/// <reference types="@astrojs/image/client" />
+
+declare module '*.svg' {
+	const content: string
+	export default content
+}
